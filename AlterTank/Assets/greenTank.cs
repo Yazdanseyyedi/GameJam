af6f9dd2e1d27ff6f3a6968ba3b9f6af8bf0815a
unknown <y.seyyedi@gmail.com>
2021-11-07 14:06:13 -1100

    add walls and implement tank move

---
+++ AlterTank/Assets/greenTank.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class greenTank : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public Rigidbody2D GreenTank;
+    private Vector3 tankDiraction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TankMove();
+    }
+
+    public void TankMove()
+    {
+        float translation = Input.GetAxis("Vertical") * moveSpeed;
+        float rotation = -Input.GetAxis("Horizontal") * rotationSpeed;
+        GreenTank.rotation += rotation * Mathf.Sign(Vector2.Dot(GreenTank.velocity, GreenTank.GetRelativeVector(Vector2.up)));
+        tankDiraction = new Vector3(GreenTank.GetRelativeVector(Vector2.up).x, GreenTank.GetRelativeVector(Vector2.up).y, 1);
+        transform.position += tankDiraction * translation;
+    }
+}
